8f539fc874f59c7a09e3bcbb75dca59a81e5e31e
TARIK <timlahi@yahoo.es>
2017-05-29 10:44:53 +0200

    funciona

--- index.php
+++ index.php
@@ -2,80 +2,23 @@
 <head>
     <title>PHP Sample</title>
 </head>
-<body>
-<?php
-// This sample uses PEAR (https://pear.php.net/package/HTTP_Request2/download)
-require_once 'HTTP/Request2.php';
+<body bgcolor="#E6E6FA">
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+<center>
+<form method="get" action="funcion.php" style="top: 1500px">
+    <font size="14"><b>Imagen URL</b>: <input type="text" name="varname" id="varname" value="varname" size="50" ><br/>
 
-// NOTE: You must use the same location in your REST call as you used to obtain your subscription keys.
-//   For example, if you obtained your subscription keys from westus, replace "westcentralus" in the
-//   URL below with "westus".
-//
-// Also, change "landmarks" to "celebrities" in the url to use the Celebrities model.
-$request = new Http_Request2('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze');
-$url = $request->getUrl();
+    <input type="submit">
+</form>
 
-$headers = array(
-    // Request headers
-    'Content-Type' => 'application/json',
+</center>
 
-    // NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
-    'Ocp-Apim-Subscription-Key' => '0a7fc5ff74e94afaa30a9c1a36516c75',
-);
-
-$request->setHeader($headers);
-
-$parameters = array(
-   // 'visualFeatures' => 'Categories,tags,description',
-    'visualFeatures' => 'Categories',
-
-);
-
-$url->setQueryVariables($parameters);
-
-$request->setMethod(HTTP_Request2::METHOD_POST);
-
-// Request body
-$body = json_encode(array(
-    // Request body parameters
-    'url' => 'https://upload.wikimedia.org/wikipedia/commons/2/23/Space_Needle_2011-07-04.jpg',
-));
-$request->setBody($body);
-
-try
-{
-    $response = $request->send();
-    echo "<pre>" . json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . "</pre>";
-
-    $data =  json_decode($response->getBody());
-
-    //print_r($data);
-
-        // Open the table
-         echo "<table>";
-         echo"<td>NOMBRE</td>";
-         echo"<td>PUNTUACION</td>";
-        // Cycle through the array
-        foreach ($data->categories as $idx => $categories) {
-
-            // Output a row
-            echo "<tr>";
-            echo "<td>$categories->name</td>";
-            echo "<td>$categories->score</td>";
-            echo "</tr>";
-        }
-
-        // Close the table
-        echo "</table>";
-
-}
-catch (HttpException $ex)
-{
-    echo "<pre>" . $ex . "</pre>";
-}
-
-
-
-?>
+<center><img src="servicioscognitivos.jpg" alt="Mountain View" style="width:500px;height:500px;"></center>
 </body>
 </html>
